7ebe966daa245dd2e849f0e5909518c611a8ac3a
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-06-22 23:00:13 -0400

    Setup ItemController and initial routes

--- Get-it-done/routes/api.php
+++ Get-it-done/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/items', [ItemController::class, 'index']);
+Route::prefix('/item')->group(function () {
+    Route::get('/store', [ItemController::class, 'store']);
+    Route::put('/{id}', [ItemController::class, 'update']);
+    Route::get('/{id}', [ItemController::class, 'destroy']);
 });
